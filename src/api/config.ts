26f74ec3fa43d26a3ae25dc6e2f2ce72a78603d4
nikita-osipov59 <nikita.osipov59@yandex.ru>
2025-11-16 15:28:39 +0300

    update api url & update version

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiBase = axios.create({
-  baseURL: "https://api.kinopoisk.dev/v1.4/",
+  baseURL: "https://api.poiskkino.dev/v1.4/",
   headers: { "X-API-KEY": import.meta.env.VITE_API_KEY },
 });
 
