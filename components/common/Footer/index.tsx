b5d4c44d2704abedad73eb6881ddd578bdc367ad
nikita-osipov59 <nikita.osipov59@yandex.ru>
2025-12-10 01:37:37 +0300

    update  link tag

--- components/common/Footer/index.tsx
+++ components/common/Footer/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { Github, Telegram } from '@/components/common';
 
 export const Footer = () => {
@@ -5,14 +7,14 @@ export const Footer = () => {
     <footer className="flex flex-col gap-2 rounded-xl border border-border p-[15px] text-center">
       <ul className="flex w-full items-center justify-center gap-[15px]">
         <li>
-          <a href="https://t.me/nikita_osipov59" target="_blank">
+          <Link href="https://t.me/nikita_osipov59" target="_blank">
             <Telegram />
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="https://github.com/nikita-osipov59/IWatch" target="_blank">
+          <Link href="https://github.com/nikita-osipov59/IWatch" target="_blank">
             <Github />
-          </a>
+          </Link>
         </li>
       </ul>
       <p>© IWatch {new Date().getFullYear()}</p>
