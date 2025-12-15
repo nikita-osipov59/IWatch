570742d671fffa4177f6a32b4cd2d012f382cb31
nikita-osipov59 <nikita.osipov59@yandex.ru>
2025-12-15 20:38:24 +0300

    update image without src

--- components/features/Search/components/SearchList/index.tsx
+++ components/features/Search/components/SearchList/index.tsx
@@ -18,7 +18,10 @@ export const SearchList = ({ data }: ListProps) => {
               <Link className="flex flex-col gap-1.5" href={ROUTER_PATH.MOVIE + `/${item.id}`}>
                 <Image
                   className="h-[300px] rounded-xl"
-                  src={item.poster.url}
+                  src={
+                    item.poster?.url ||
+                    'https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg'
+                  }
                   alt={item.name}
                   width={200}
                   height={300}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ hostname: 'avatars.mds.yandex.net' }],
+    remotePatterns: [{ hostname: 'avatars.mds.yandex.net' }, { hostname: 'upload.wikimedia.org' }],
   },
 };
 
