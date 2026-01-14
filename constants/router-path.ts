87b69dd7d0675d7d2cf625478857fd07bbd08a95
nikita-osipov59 <nikita.osipov59@yandex.ru>
2026-01-14 16:07:12 +0300

    add new paths

--- constants/router-path.ts
+++ constants/router-path.ts
@@ -2,6 +2,9 @@ export const ROUTER_PATH = {
   HOME: '/',
   SIGNIN: '/signin',
   SIGNUP: '/signup',
+  RECOVERYPASSWORD: '/recovery',
+  UPDATEPASSWORD: '/recovery/update',
   SEARCH: '/search',
   MOVIE: '/movie',
+  PROFILE: '/profile',
 };
