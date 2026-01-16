166b5d05b19412fe53f7a255c88854898de78661
nikita-osipov59 <nikita.osipov59@yandex.ru>
2026-01-16 16:37:35 +0300

    add size props

--- components/common/LogOut/index.tsx
+++ components/common/LogOut/index.tsx
@@ -2,13 +2,17 @@ import { LogOutIcon } from 'lucide-react';
 
 import { signOut } from '@/actions/auth';
 
-export const LogOut = () => {
+type LogOutTypes = {
+  size?: number;
+};
+
+export const LogOut = ({ size }: LogOutTypes) => {
   return (
     <button
       onClick={() => signOut()}
-      className="flex w-fit cursor-pointer items-center gap-3 rounded p-2 transition-all duration-300 hover:text-foreground"
+      className="flex w-fit cursor-pointer items-center gap-3 rounded transition-all duration-300 hover:text-foreground"
     >
-      <LogOutIcon />
+      <LogOutIcon size={size} />
       <h1>Logout</h1>
     </button>
   );
