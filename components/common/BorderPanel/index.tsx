aa482d00e17686d6e22563f6ef936ba623d71afb
nikita-osipov59 <nikita.osipov59@yandex.ru>
2026-01-18 17:26:36 +0300

    update BorderPanel

--- components/common/BorderPanel/index.tsx
+++ components/common/BorderPanel/index.tsx
@@ -1,13 +1,28 @@
 interface BorderPanelProps {
   children: React.ReactNode;
   title?: string;
+  subTitle?: string;
+  className?: string;
+  classNameTitle?: string;
+  classNameSubTitle?: string;
   classNameChildren?: string;
 }
 
-export const BorderPanel = ({ children, title, classNameChildren }: BorderPanelProps) => {
+export const BorderPanel = ({
+  children,
+  title,
+  subTitle,
+  className,
+  classNameTitle,
+  classNameSubTitle,
+  classNameChildren,
+}: BorderPanelProps) => {
   return (
-    <div className="flex h-fit flex-col gap-[15px] rounded-xl border border-border bg-background-info p-[15px]">
-      {title && <p className="text-accent">{title}</p>}
+    <div
+      className={`flex h-fit flex-col gap-[15px] rounded-xl border border-border bg-background-info p-[15px] ${className}`}
+    >
+      {title && <p className={`text-accent ${classNameTitle}`}>{title}</p>}
+      {subTitle && <p className={classNameSubTitle}>{subTitle}</p>}
       {children && (
         <div className={`text-main duration-300 ${classNameChildren ?? ''}`}>{children}</div>
       )}
