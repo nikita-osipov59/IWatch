b00b71723bea15764f0c0a9f91702ae9b5ff8c3f
nikita-osipov59 <nikita.osipov59@yandex.ru>
2026-02-05 12:20:42 +0300

    update width for home layout update name to HomeLayout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: 'Главная',
 };
 
-export default async function RootLayout({
+export default async function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <div className="flex">
       <Header />
-      <div className="ml-[150px] flex min-h-screen w-full flex-col gap-[15px] p-[15px]">
+      <div className="ml-[150px] flex min-h-screen w-[-webkit-fill-available] flex-col gap-[15px] p-[15px]">
         <div className="flex items-center justify-between">
           <Search />
           <UserPanelExtended user={user} profile={userWithProfile} />
