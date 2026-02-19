2cffd299444310e5b4ca8338ea9d41b2ed58fe38
nikita-osipov59 <nikita.osipov59@yandex.ru>
2026-02-19 16:21:39 +0300

    update Genres

--- components/features/Genres/index.tsx
+++ components/features/Genres/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { BorderPanel } from '@/components/common';
 import { useGetQueryGenres } from './mutations';
 import { ROUTER_PATH } from '@/constants';
+import { formatTitle } from '@/utils/helpers';
 
 export const Genres = () => {
   const { data } = useGetQueryGenres();
@@ -16,7 +17,7 @@ export const Genres = () => {
               className="flex h-full items-center rounded-xl bg-black p-[40px]"
               href={ROUTER_PATH.SELECTION + `/${item.name}`}
             >
-              {item.name}
+              {formatTitle(item.name)}
             </Link>
           </li>
         ))}
